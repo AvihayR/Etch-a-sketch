3beee8e4059532dbfd27b6f60dbe5cee4d0d0b04
AvihayR <avihayrabi@gmail.com>
2022-10-29 05:09:14 -0700

    Fix grid to hold exactly 16 columns as asked for in 'TheOdinProject'

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ function createDivs(){
         const div = document.createElement('div');
         div.classList.add('grid-item')
         div.textContent = i;
-        gridContainer.appendChild(div)
-        if( i % 16 == 0){
-            let br = document.createElement('div')
-            br.classList.add(".block-break")
-            gridContainer.appendChild(br);
-            console.log('16');
-        };
+        gridContainer.appendChild(div);
     };
 };
 
