1fdf71dd07e4d87922be02ac163be0a83f0d455b
AvihayR <avihayrabi@gmail.com>
2022-10-29 11:13:27 -0700

    Add default grid button

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const gridContainer = body.querySelector('.grid-container');
 const btnContainer = body.querySelector('.btn-container');
 const customBtn = btnContainer.querySelector('.custom-btn');
 const resetBtn = btnContainer.querySelector('.reset');
+const defaultBtn = btnContainer.querySelector('.default-btn');
 
 function createDivs(){
+    gridContainer.replaceChildren();
     for(let i = 0; i < 256; i ++){
         const div = document.createElement('div');
         div.classList.add('grid-item')
         div.textContent = i;
         gridContainer.appendChild(div);
-        //gridContainer.style = "grid-template-columns: repeat(6,1fr)"
+        gridContainer.style = `grid-template-columns: repeat(16,1fr)`;
+        let gridItems = gridContainer.querySelectorAll('.grid-item');
+        gridItems.forEach(item => item.addEventListener('mouseover',colorDiv))
+        
     };
 };
 
@@ -25,19 +30,19 @@ function colorDiv(e){
 gridItems.forEach( item => item.addEventListener('mouseover',colorDiv));
 
 function resetColor(){
+    let gridItems = gridContainer.querySelectorAll('.grid-item');
     gridItems.forEach(item => item.classList.remove('color'));
-}
+};
 
 function createCustomDivs(){
     let num = prompt('Enter a single number of desired Columns, up to a maximum of 100. ');
+
+
     if (num >= 101 || isNaN(num) || num<0){
         alert('Error, input is not a number or number is exceeding 100.');
         return;
     };
-
-
     gridContainer.replaceChildren();
-
         for(let i = 0; i<num*num; i++){
             const div = document.createElement('div');
             div.classList.add('grid-item');
@@ -51,3 +56,4 @@ function createCustomDivs(){
 
 customBtn.addEventListener('click',createCustomDivs);
 resetBtn.addEventListener('click',resetColor);
+defaultBtn.addEventListener('click',createDivs);
