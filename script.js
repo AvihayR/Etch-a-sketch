af71b7bf63da21910e5eae81231344c57f7ab1e6
AvihayR <avihayrabi@gmail.com>
2022-10-29 10:05:41 -0700

    Add createCustomDivs func, allows user to choose how many rows x columns will be in the grid

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function createDivs(){
         div.classList.add('grid-item')
         div.textContent = i;
         gridContainer.appendChild(div);
+        //gridContainer.style = "grid-template-columns: repeat(6,1fr)"
     };
 };
 
@@ -19,3 +20,28 @@ function colorDiv(e){
 };
 
 gridItems.forEach( item => item.addEventListener('mouseover',colorDiv));
+
+
+const btnContainer = body.querySelector('.btn-container');
+const customBtn = btnContainer.querySelector('.custom-btn');
+
+function createCustomDivs(){
+    let num = prompt('Enter a single number of desired Columns, e.g "16" ');
+    if (num >= 101 || num == NaN){
+        alert('Error, input is not a number or number is exceeding 100.')
+        return;
+    };
+    gridContainer.replaceChildren();
+
+        for(let i = 0; i<num*num; i++){
+            const div = document.createElement('div');
+            div.classList.add('grid-item');
+            div.textContent = i;
+            gridContainer.appendChild(div);
+            gridContainer.style = `grid-template-columns: repeat(${num},1fr)`;
+        };
+        let gridItems = gridContainer.querySelectorAll('.grid-item');
+        gridItems.forEach(item => item.addEventListener('mouseover',colorDiv))
+};
+
+customBtn.addEventListener('click',createCustomDivs);
